b6a68d74ead812dcad5ddd88fd1c91bbd259a01f
quentinfougereau <quentin.fougereau@gmail.com>
2019-11-24 23:31:27 +0100

    add Control, KeyListener and KeyBoardControl class

---
+++ src/main/java/fr/univ_amu/io_engine/Control.java
@@ -0,0 +1,45 @@
+package fr.univ_amu.io_engine;
+
+public class Control {
+
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
+
+    public Control() {
+
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+}
