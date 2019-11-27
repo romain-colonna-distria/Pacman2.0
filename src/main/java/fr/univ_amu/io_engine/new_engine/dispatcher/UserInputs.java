8fa29acb0a944efcbb08052463e3e7706a3a7673
Romain Colonna D'istria <romain.colonna-distria@etu.univ-amu.fr>
2019-11-27 02:16:01 +0100

    test nouveau moteur io

---
+++ src/main/java/fr/univ_amu/io_engine/new_engine/dispatcher/UserInputs.java
@@ -0,0 +1,8 @@
+package fr.univ_amu.io_engine.new_engine.dispatcher;
+
+
+public interface UserInputs {
+    //void addObserver(UserInputsObserver observer);
+    //void removeObserver(UserInputsObserver observer);
+    void dispatch(String key);
+}
